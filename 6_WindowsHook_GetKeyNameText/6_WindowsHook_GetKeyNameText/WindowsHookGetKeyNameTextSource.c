4cf775f8c2902640d8c5a6a98d26b0b92e3be5d6
MeseryGG <Meserygamer@gmail.com>
2023-12-26 00:26:42 +0300

    Задание 6 Сделано

---
+++ 6_WindowsHook_GetKeyNameText/6_WindowsHook_GetKeyNameText/WindowsHookGetKeyNameTextSource.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <windows.h>
+
+LRESULT CALLBACK KeyBoardCallBack(int code, WPARAM wParam, LPARAM lParam)
+{
+	PKBDLLHOOKSTRUCT HookData = (PKBDLLHOOKSTRUCT)lParam;
+	if (code == HC_ACTION && (wParam == WM_KEYDOWN || wParam == WM_SYSKEYDOWN))
+	{
+		//GetForegroundWindow возвращает дескриптор активного в данный момент окна
+		//GetWindowThreadProcessId возвращает ID данного процесса
+		//GetKeyboardLayout возвращает раскладку клавиатуры, используемую в этом процессе
+		DWORD iKey = MapVirtualKeyW(HookData->vkCode, NULL) << 16;
+		if (!(HookData->vkCode <= 1 << 5)) // 32 (т.к. первые 32 символа являются не печатными)
+			iKey |= 0x1 << 24; //Задаём истину для 24 бита (|= - побитовое присваивание a|= b эквивалентно a = a|b)
+		wchar_t* buffer = calloc(256, sizeof(wchar_t));
+		GetKeyNameTextW(iKey, buffer, 256);
+		MessageBox(0, buffer, L"Нажали на клавишу!", MB_OK);
+	}
+	return CallNextHookEx(NULL, code, wParam, lParam);
+}
+
+int main()
+{
+	HHOOK descHook = SetWindowsHookExW(WH_KEYBOARD_LL, KeyBoardCallBack, NULL, 0); 
+	MSG msg;
+	while (GetMessageW(&msg, NULL, 0, 0)) //Извлекает сообщение из очереди сообщений
+	{
+		TranslateMessage(&msg); //Преобразует сообщения с виртуальным ключом в символьные сообщения
+		DispatchMessageW(&msg); //Отправляет сообщение в оконную процедуру (В нашу функцию)
+	}
+	UnhookWindowsHookEx(descHook);
+}
