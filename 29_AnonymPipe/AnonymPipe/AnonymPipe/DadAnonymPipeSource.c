665ac5f79cc7e2f150ab381df9a418b1a929f329
MeseryGG <Meserygamer@gmail.com>
2023-12-25 12:07:49 +0300

    Сделано задание 29 на Windows

---
+++ 29_AnonymPipe/AnonymPipe/AnonymPipe/DadAnonymPipeSource.c
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+	system("chcp 1251>cls");
+	HANDLE READPIPE, WRITEPIPE;
+	SECURITY_ATTRIBUTES Sec = { sizeof(LPSECURITY_ATTRIBUTES), NULL, TRUE };
+	CreatePipe(&READPIPE, &WRITEPIPE, &Sec, 256); //Канал в 256 байт
+	char* buffer = calloc(20, sizeof(char));
+	sprintf(buffer, "%d %d\0", READPIPE, WRITEPIPE); //формирование аргументов командной строки
+	STARTUPINFO* SU = calloc(1, sizeof(STARTUPINFO));
+	SU->cb = sizeof(STARTUPINFO);
+	PROCESS_INFORMATION* PI = calloc(1, sizeof(PROCESS_INFORMATION));
+	printf("Чтение - %u\nЗапись - %u", READPIPE, WRITEPIPE);
+	CreateProcessA("AnonymPipeSon.exe"
+				  ,buffer
+				  ,NULL
+				  ,NULL
+				  ,TRUE //TRUE для наследования дескрипторов от родителя
+				  ,0
+		          ,NULL
+		          ,NULL
+		          ,SU
+	              ,PI);
+	free(buffer);
+	int* testWritenBytes = calloc(1, sizeof(int));
+	WriteFile(WRITEPIPE, "Проверка связи в канале", 44, testWritenBytes, NULL);
+	printf("\nЯ записал - %d", *testWritenBytes);
+	WaitForSingleObject(PI->hProcess, INFINITE);
+	int* sonExitCode = calloc(1, sizeof(int));
+	GetExitCodeProcess(PI->hProcess, sonExitCode);
+	CloseHandle(PI->hProcess);
+	CloseHandle(PI->hThread);
+	printf("Код завершения дочернего процесса - %d\n", *sonExitCode);
+	system("pause");
+}
