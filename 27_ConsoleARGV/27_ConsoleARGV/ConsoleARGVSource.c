91b0c0299bf5f3cca43f08cc0f4b2d390f07de3a
MeseryGG <Meserygamer@gmail.com>
2023-12-26 21:03:38 +0300

    Задание 27 на Windows сделано

---
+++ 27_ConsoleARGV/27_ConsoleARGV/ConsoleARGVSource.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int count, char* argv[])
+{
+	system("chcp 1251>nul");
+	for (int i = 0; i < count; i++)
+	{
+		printf("Аргумент №%d - %s\n", i+1, argv[i]);
+	}
+	getchar();
+}
